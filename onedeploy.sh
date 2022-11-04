0a0c46fbd865b4e079d36b8c4825a8a76e58e503
Benoit Moussaud <benoit@moussaud.org>
2022-11-04 11:34:08 +0100

    fix onedeploy

--- onedeploy.sh
+++ onedeploy.sh
@@ -3,7 +3,7 @@ make tanzu-cluster-essentials
 make tap
 watch kubectl get app -A
 make tap-gui-ip
-tanzu acc create micropet-java-service-accelerator --git-repo https://github.com/bmoussaud/micropet-java-service-accelerator --git-branch main --interval 5s
-tanzu acc create micropet-golang-service-accelerator --git-repo https://github.com/bmoussaud/micropets-golang-service-accelerator --git-branch main --interval 5s
+tanzu acc create micropets-java-service-accelerator --git-repo https://github.com/bmoussaud/micropet-java-service-accelerator --git-branch main --interval 5s
+tanzu acc create micropets-golang-service-accelerator --git-repo https://github.com/bmoussaud/micropets-golang-service-accelerator --git-branch main --interval 5s
 
 make postgres-tanzu-operator
